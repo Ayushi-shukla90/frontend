52c33549e6746e0b46cf8cf524ff6c37278f75e1
Ayushi-shukla90 <ayushishukla66237@gmail.com>
2025-08-27 20:22:21 +0530

    recent

---
+++ javascript/index.js
@@ -0,0 +1,20 @@
+//  var a = 10
+//  console.log(a)
+//  let b = 8
+//  b = 9
+//  console.log(b)
+
+// console.log(5 == '5')
+// console.log(5 === '5')
+
+// let userName = "Ayushi"
+// let lastName = "Shukla"
+// console.log(`${userName}   ${lastName}`)
+let a = true 
+if(a){
+    console.log("hello")
+}else{
+    console.log("hiiii")
+}
+let b = false
+b ? console.log("hello"):console.log("byeee")
